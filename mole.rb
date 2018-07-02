b76e391a779c5c174db3e187fd9620ccb7b9846a
Shritesh99 <Shritesh.sj@gmail.com>
2018-07-03 01:42:27 +0530

    Added Mole visibility feture

--- mole.rb
+++ mole.rb
@@ -9,8 +9,9 @@ class Mole < Gosu::Window
     @y = 200
     @width = 100
     @hieght = 75
-    @velocity_x = 1
-    @velocity_y = 1
+    @velocity_x = 5
+    @velocity_y = 5
+    @visible = 0
   end
   def draw
     @image.draw(@x - @width / 2,@y - @hieght / 2, 1)
@@ -20,5 +21,7 @@ class Mole < Gosu::Window
     @velocity_x *= -1 if @x + @width / 2 > 800 || @x + @width / 2 < 0
     @y += @velocity_y
     @velocity_y *= -1 if @y + @hieght / 2 > 600 || @y + @hieght / 2 < 0
+    @visible -= 1
+    @visible = 50 if @visible < -10 && rand < 0.01
   end
 end
