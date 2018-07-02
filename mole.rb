b4fd5c524fe46381442e49007fa77d5dbb43f75d
Shritesh99 <Shritesh.sj@gmail.com>
2018-07-03 02:24:35 +0530

    Almost Completed game

--- mole.rb
+++ mole.rb
@@ -14,6 +14,9 @@ class Mole < Gosu::Window
     @velocity_y = 5
     @visible = 0
     @hit = 0
+    @font = Gosu::Font.new(30)
+    @score = 0
+    @playing = true
   end
   def draw
     if @visible > 0
@@ -29,21 +32,36 @@ class Mole < Gosu::Window
     end
     draw_quad(0,0,c,800,0,c,800,600,c,0,600,c)
     @hit = 0
+    @font.draw(@score.to_s, 650, 50, 2)
+    @font.draw(@time_left.to_s, 50, 50, 2)
+    if !@playing
+      @font.draw("Game Over!!", 300, 300, 3)
+      @visible = 20
+      @time_left = 0
+    end
   end
   def update
-    @x += @velocity_x
-    @velocity_x *= -1 if @x + @width / 2 > 800 || @x + @width / 2 < 0
-    @y += @velocity_y
-    @velocity_y *= -1 if @y + @hieght / 2 > 600 || @y + @hieght / 2 < 0
-    @visible -= 1
-    @visible = 50 if @visible < -10 && rand < 0.01
+   if @playing
+     @x += @velocity_x
+     @velocity_x *= -1 if @x + @width / 2 > 800 || @x + @width / 2 < 0
+     @y += @velocity_y
+     @velocity_y *= -1 if @y + @hieght / 2 > 600 || @y + @hieght / 2 < 0
+     @visible -= 1
+     @visible = 50 if @visible < -10 && rand < 0.01
+     @time_left = (60 - (Gosu.milliseconds / 1000))
+     @playing = false if @time_left < 1
+   end
   end
   def button_down(id)
-    if(id == Gosu::MsLeft)
-      if Gosu.distance(mouse_x, mouse_y, @x, @y) < 50 && @visible >= 0
-        @hit = 1
-      else
-        @hit = -1
+    if @playing
+      if(id == Gosu::MsLeft)
+        if Gosu.distance(mouse_x, mouse_y, @x, @y) < 50 && @visible >= 0
+          @hit = 1
+          @score += 10
+        else
+          @hit = -1
+          @score -= 3
+        end
       end
     end
   end
